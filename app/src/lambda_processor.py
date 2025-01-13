6137f7c53737a543840e14cf80eaaca8d9691173
Vinicius Petroli Affonso <126300608+viniciuspetroli@users.noreply.github.com>
2025-01-13 17:25:30 -0300

    changed: better prompt, changed get method to get user_id by path

--- app/src/lambda_processor.py
+++ app/src/lambda_processor.py
@@ -1,3 +1,4 @@
+import re
 import json
 from src.dynamo import DynamoService
 from src.utils import utils
@@ -5,11 +6,11 @@ from src.prompt import gemini_prompt
 
 def process(event, dynamo_service: DynamoService):
     http_method = event["httpMethod"]
-    body = json.loads(event["body"]) if "body" in event else {}
 
     if http_method == "GET":
+        user_id = event["pathParameters"]["user_id"]
         key = {
-            'user_id': {'N': body["user_id"]},
+            'user_id': {'N': user_id},
         }
         user_data = dynamo_service.get_item(key=key)
         user_fav_cat = user_data['Item']['user_fav_cat']['S']
@@ -18,8 +19,11 @@ def process(event, dynamo_service: DynamoService):
         prompt = gemini_prompt.generate_prompt(user_fav_cat, user_fav_aut)
 
         call_gemini_api = utils.call_gemini_api(prompt)
-        text = call_gemini_api["candidates"][0]["content"]["parts"][0]["text"]
-        result = {"text": text}
+        text = json.dumps(call_gemini_api["candidates"][0]["content"]["parts"][0]["text"])
+        result = {
+            "statusCode": 200,
+            "body": text
+        }
     else:
         raise Exception("Invalid HTTP method")
     return result
--- app/src/prompt/gemini_prompt.py
+++ app/src/prompt/gemini_prompt.py
@@ -12,7 +12,18 @@ def generate_prompt(fav_cat, fav_aut):
     Não pode ser pintura
     Deve ser somentes livros que existem, e foram publicados.
     Não estenda muito a mensagem, simplesmente me dê a lista.
+
+    Não use acentos nem caracteres especiais nos nomes dos livros nem dos autores.
+    Não use cedilha nos nomes dos livros e dos autores (ç), não use acentos, não use apóstrofes, use somente caracteres existentes no teclado US americano
+    Por exemplo enriqueça, retorne enriqueca, o que for cedilha pode usar o C, e o que for acento pode ignorar e colocar somente o caractere.
+    *não use cedilha, troque o cedilha por c*
+    *se houver cedilha no nome do livro ou do autor, troque por C*
+    *não use ç, troque o ç por c*
+    *não use acentos, escreva somente o caracter*
+    Nome de livros sempre em portugues
+
     A lista pode ser da seguinte forma:
-    'Livros que você pode gostar baseado nas suas preferências:'
+    '- Livro 1, Autor 1
+     - Livro 2, Autor 2'
     '''
     return prompt
